376a9bf1969c901c35edd461514c17058b7d612e
johnjoel061 <alfabetejohnjoel@gmail.com>
2025-05-13 21:02:10 +0800

    ...

--- BaseLibrary/Entities/Employee.cs
+++ BaseLibrary/Entities/Employee.cs
@@ -1,12 +1,17 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿
 
 namespace BaseLibrary.Entities
 {
-    internal class Employee
+    public class Employee : BaseEntity
     {
+        public string? CivilId { get; set; }
+        public string? FileNumber { get; set; }
+        public string? Fullname { get; set; }
+        public string? JobName { get; set; }
+        public string? Address { get; set; }
+        public string? TelephoneNumber { get; set; }
+        public string? Photo { get; set; }
+        public string? Other { get; set; }
+
     }
 }
